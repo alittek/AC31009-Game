5c785d9367e7d7aa8d6b42964fbe6072eb48f927
alittek <43140594+alittek@users.noreply.github.com>
2021-04-14 11:16:49 +0200

    steal artifacts functionality
    enemies can steal a number of artifacts from a player before they disappear.
    Player dies when all artifacts are lost

--- Player.gd
+++ Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
+var artifacts : int = 3
 var speed = 100
 var look_direction = "down"
 onready var animation = $AnimationPlayer
-
 onready var rayCast = $RayCast2D
 
 func _physics_process(_delta):
@@ -33,3 +33,17 @@ func _physics_process(_delta):
 	elif movement == Vector2.ZERO:
 		animation.stop(false)
 		#animation.play(str("Idle_", look_direction ))
+
+# TODO player picks up items
+func get_artifact(amount):
+	artifacts += amount
+
+# player meets enemy
+func lose_artifact(amount):
+	artifacts -= amount
+	if artifacts <= 0:
+		die()
+		
+func die():
+	#TODO back to menu?
+	get_tree().reload_current_scene()
