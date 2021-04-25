366b94165f9a05989db2e82fccc0dc7325f92915
alittek <43140594+alittek@users.noreply.github.com>
2021-04-25 13:03:59 +0200

    read scores from file, display sorted

--- FileSystem.gd
+++ FileSystem.gd
@@ -1,28 +1,42 @@
 extends Node
 
 var score_file = "user://score.save"
-var highscore
+var password = "pass"
+#var highscore
 var status
+var highscores
+
+
+func start():
+	save_score(3)
+	save_score(4)
+	save_score(100)
+	save_score(12)
 
 func save_score(highscore):
 	var file = File.new()
 	if file.file_exists(score_file):
+		#status = file.open_encrypted_with_pass(score_file, File.READ_WRITE, OS.get_unique_id())
 		status = file.open(score_file, File.READ_WRITE)
+		#status = file.open_encrypted_with_pass(score_file, File.READ_WRITE, password)
 	else:
+		#status = file.open_encrypted_with_pass(score_file, File.WRITE, OS.get_unique_id())
 		status = file.open(score_file, File.WRITE)
+		#status = file.open_encrypted_with_pass(score_file, File.WRITE, password)
 	
 	if status == OK:
 		file.seek_end()
-		file.store_string(str(highscore) + "\r")
+		file.store_string(str(highscore) + ",")
 		file.close()
-
 
 func load_score():
 	var file = File.new()
 	if file.file_exists(score_file):
 		file.open(score_file, File.READ)
-		highscore = file.get_as_text()
+		#file.open_encrypted_with_pass(score_file, File.READ, password)
+		highscores = file.get_as_text()
+		#create_scores(highscore)
 		file.close()
 	else:
-		highscore = 0
-	return highscore
+		return
+	return highscores
--- GameScore.gd
+++ GameScore.gd
@@ -2,14 +2,37 @@ extends Node2D
 
 onready var score : Label = get_node("Score/Label")
 var file = load("res://FileSystem.gd").new()
+var intArray = []
 
 func _ready():
-	display_score()
+	# can be removed, adds values to file
+	file.start()
+	var scores = file.load_score()
+	create_scores(scores)
 
 func display_score():
 	$Back.color = Color.greenyellow
-	var newScore = file.load_score()
-	score.text = str(newScore)
+	# print top 5 scores
+	for i in range(intArray.size()):
+		print(intArray[i])
+		score.text += str(intArray[i])
+	
+
+# create the array with the top five scores
+func create_scores(score):
+	# split string of scores into array
+	var scoreArray = score.split(",", true, 0)
+	# remove last empty index
+	scoreArray.remove(scoreArray.size()-1)
+	
+	# change each item in array into int and add to new array
+	for i in range(scoreArray.size()):
+		var newscore = int(scoreArray[i])
+		intArray.append(newscore)
+	
+	# sort array high to low
+	intArray.sort()	
+	display_score()
 
 func _input(event):
 	if Input.is_action_just_pressed("interact"):
